0803da54bd143c5006a7d801ac800efd0d3d9afb
Flochlay <sebastien.flochlay@gmail.com>
2020-04-07 22:52:48 +0200

    feat(chat): add feedback in case of empty message

--- src/components/NewMessage.vue
+++ src/components/NewMessage.vue
@@ -3,29 +3,36 @@
     <form @submit.prevent="addMessage">
       <label for="new-message">New message (press enter to add):</label>
       <input type="text" name="new-message" v-model="newMessage" />
+      <p v-if="feedback" class="red-text">{{ feedback }}</p>
     </form>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'NewMessage',
-      props: {
+  name: "NewMessage",
+  props: {
     name: String
   },
   data() {
-      return {
-          newMessage: null
-      }
+    return {
+      newMessage: null,
+      feedback: null
+    };
   },
   methods: {
-      addMessage(){
-          console.log(this.newMessage, this.name, Date.now())
+    addMessage() {
+      if (!this.newMessage) {
+        this.feedback = "You must enter a message in order to send one";
+        return
       }
+
+      this.feedback = null;
+      console.log(this.newMessage, this.name, Date.now());
+    }
   }
-}
+};
 </script>
 
 <style>
-    
 </style>
